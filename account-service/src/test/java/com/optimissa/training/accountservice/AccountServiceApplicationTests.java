bef5086e615ff22660d5bba667ba83aeef518351
OPTIMISSA\antuanel.medina <antuanel.medina@optimissa.com>
2023-04-18 17:22:37 +0200

    [#28255] Created structured account-service

---
+++ account-service/src/test/java/com/optimissa/training/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimissa.training.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
