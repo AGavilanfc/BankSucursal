547b66c3b2efcd5118812953ecb886b70a2b43d1
OPTIMISSA\javier.catalan <javier.catalan@optimissa.com>
2023-04-18 17:25:52 +0200

    [#28256] Created structure fund-service

---
+++ fund-service/src/test/java/com/optimissa/training/fundservice/FundServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimissa.training.fundservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
