0a05c353a6fc3f00cc2d18064e3a81818196dd05
OPTIMISSA\alberto.carrillo <alberto.carrillo@optimissa.com>
2023-04-24 17:33:45 +0200

    ﻿[#28264] add log to getUsers

--- user-service/src/main/java/com/optimissa/training/userservice/controller/UserController.java
+++ user-service/src/main/java/com/optimissa/training/userservice/controller/UserController.java
@@ -2,21 +2,34 @@ package com.optimissa.training.userservice.controller;
 
 import com.optimissa.training.userservice.model.User;
 import com.optimissa.training.userservice.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @Autowired
     UserService userService;
 
     @GetMapping("")
     public List<User> getUsers() {
-        return userService.getUsers();
+        List<User> users = null;
+        logger.info("Inside method getUsers()");
+        long startTime = System.nanoTime();
+        users = userService.getUsers();
+        long endTime = System.nanoTime();
+        logger.info("Call to userService.getUsers(). Response:" + users.toString() +
+                ". Execution took: " + (endTime-startTime)/1000000 + "ms");
+        return users;
     }
 
     @GetMapping("/{id}")
