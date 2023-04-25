fdd46c0bfc4c8f2c676e4eef751269bf30406580
OPTIMISSA\ignacio.pinto <ignacio.pinto@optimissa.com>
2023-04-25 12:59:33 +0200

    [#28269] fixed log transactions and implement code error

--- transaction-service/src/main/java/com/optimissa/training/transactionservice/controllers/TransactionsController.java
+++ transaction-service/src/main/java/com/optimissa/training/transactionservice/controllers/TransactionsController.java
@@ -5,41 +5,50 @@ import com.optimissa.training.transactionservice.services.TransactionsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
-public class TransactionsController {
+public class TransactionsController{
 
     private static final Logger log = LoggerFactory.getLogger(TransactionsController.class);
+    RestTemplate restTemplate = new RestTemplate();
+    String urlFund = "http://localhost:8095/funds";
+    String urlAccount = "http://localhost:8096/api/some-endpoint";
 
 
     @Autowired
     private TransactionsService tranService;
 
     @GetMapping("/{id}")
-    public Transactions getTransaction(@PathVariable int id) {
-        log.info("getTransaction: " + id);
+    public ResponseEntity<Transactions> getTransaction(@PathVariable int id) {
 
-        try{
-           return tranService.getByIdTransaction(id);
-        }catch (Exception e){
-            log.error("Error: " + e.getMessage());
+        try {
+            if(ResponseEntity.ok(tranService.getByIdTransaction(id)) != null){
+                log.info("obtain one transaction: {}", id);
+                return ResponseEntity.ok(tranService.getByIdTransaction(id));
+            }
+        } catch (Exception e) {
+            log.error("Error:{} {}", ResponseEntity.internalServerError().build(), e.getMessage());
 
         }
-        return null;
+        return ResponseEntity.internalServerError().build();
 
 
     }
+
     @GetMapping("")
     public List<Transactions> getAllTransaction() {
-        log.info("getAllTransaction");
-        try{
+        log.info("obtain all transactions");
+        try {
             return tranService.getAllTransactions();
-        }catch (Exception e){
-            log.error("Error: " + e.getMessage());
+        } catch (Exception e) {
+            log.error("Error: {}", e.getMessage());
 
         }
 
@@ -47,23 +56,38 @@ public class TransactionsController {
     }
 
     @PostMapping
-    public Object createTransaction(@RequestBody Transactions transaction) {
-        Long start=System.currentTimeMillis();
-        log.info("createTransaction: " + transaction);
-        try{
-            return tranService.insertNewTransaction(transaction);
-        }catch (Exception e){
-            log.error("Error: " + e.getMessage());
+    public ResponseEntity<Integer> createTransaction(@RequestBody Transactions transaction) {
+        Long start = System.currentTimeMillis();
+        try {
+            if(ResponseEntity.ok(tranService.insertNewTransaction(transaction)) != null
+                    && ResponseEntity.ok(tranService.insertNewTransaction(transaction)) != null){
+                log.info("create transaction: {}", transaction);
+                return ResponseEntity.ok(tranService.insertNewTransaction(transaction));
+            }
+            ResponseEntity.ok(tranService.insertNewTransaction(transaction));
+        } catch (Exception e) {
+            log.error("Error: {}", ResponseEntity.internalServerError().build(), e.getMessage());
 
         }
-        Long end=System.currentTimeMillis();
-        log.info("Time: " + (end-start));
+        Long end = System.currentTimeMillis();
+        log.info("Time: {}", (end - start));
 
-        return null;
+        return ResponseEntity.internalServerError().build();
     }
 
 
+    @GetMapping("/fund/{id}")
+    public Object getFund(@PathVariable int id) {
+        log.info("obtain one fund: {}", id);
 
-    
+        try {
+            return restTemplate.getForObject(urlFund + "/" + id, Object.class);
+        } catch (Exception e) {
+            log.error("Error: {}", e.getMessage());
 
+
+
+        }
+        return null;
+    }
 }
