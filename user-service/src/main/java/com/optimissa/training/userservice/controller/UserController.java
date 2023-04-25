b69033fb30f2a90e9fdc1f86992af7c02b309e9e
OPTIMISSA\alberto.carrillo <alberto.carrillo@optimissa.com>
2023-04-25 09:07:04 +0200

    [#28264] add log for every method in class controller

--- user-service/src/main/java/com/optimissa/training/userservice/controller/UserController.java
+++ user-service/src/main/java/com/optimissa/training/userservice/controller/UserController.java
@@ -24,32 +24,55 @@ public class UserController {
     public List<User> getUsers() {
         List<User> users = null;
         logger.info("Inside method getUsers()");
-        long startTime = System.nanoTime();
+        long startTime = System.currentTimeMillis();
         users = userService.getUsers();
-        long endTime = System.nanoTime();
-        logger.info("Call to userService.getUsers(). Response:" + users.toString() +
-                ". Execution took: " + (endTime-startTime)/1000000 + "ms");
+        long endTime = System.currentTimeMillis();
+        logger.info("Call to userService.getUsers(). Response: {}. Execution took: {}ms", users.toString(), endTime-startTime);
         return users;
     }
 
     @GetMapping("/{id}")
-    public User getUsers(@PathVariable int id) {
-        return userService.getUser(id);
+    public User getUser(@PathVariable int id) {
+        User user = null;
+        logger.info("Inside method getUser()");
+        long startTime = System.currentTimeMillis();
+        user = userService.getUser(id);
+        long endTime = System.currentTimeMillis();
+        logger.info("Call to userService.getUser(). Response: {}. Execution took: {}ms", user.toString(), endTime-startTime);
+        return user;
     }
 
     @PostMapping()
     public int addUser(@RequestBody User user) {
-        return userService.addUser(user);
+        int response = 0;
+        logger.info("Inside method addUser()");
+        long startTime = System.currentTimeMillis();
+        response = userService.addUser(user);
+        long endTime = System.currentTimeMillis();
+        logger.info("Call to userService.addUser(). Response: Affected rows {}. Execution took: {}ms", response, endTime-startTime);
+        return response;
     }
 
     @PutMapping("/{id}")
     public int modifyUser(@RequestBody User user, @PathVariable int id) {
-        return userService.modifyUser(user, id);
+        int response = 0;
+        logger.info("Inside method modifyUser()");
+        long startTime = System.currentTimeMillis();
+        response = userService.modifyUser(user, id);
+        long endTime = System.currentTimeMillis();
+        logger.info("Call to userService.modifyUser(). Response: Affected rows {}. Execution took: {}ms", response, endTime-startTime);
+        return response;
     }
 
     @DeleteMapping("/{id}")
     public int removeUser(@PathVariable int id) {
-        return userService.removeUser(id);
+        int response = 0;
+        logger.info("Inside method removeUser()");
+        long startTime = System.currentTimeMillis();
+        response = userService.removeUser(id);
+        long endTime = System.currentTimeMillis();
+        logger.info("Call to userService.removeUser(). Response: Affected rows {}. Execution took: {}ms", response, endTime-startTime);
+        return response;
     }
 
 }
