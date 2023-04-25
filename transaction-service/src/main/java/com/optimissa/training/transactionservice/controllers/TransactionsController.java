b0c8c7b758ba2766ea6850f5adf3288a7902e931
OPTIMISSA\ignacio.pinto <ignacio.pinto@optimissa.com>
2023-04-25 10:22:26 +0200

    [#28269] logs created and control errors with exceptions

--- transaction-service/src/main/java/com/optimissa/training/transactionservice/controllers/TransactionsController.java
+++ transaction-service/src/main/java/com/optimissa/training/transactionservice/controllers/TransactionsController.java
@@ -1,36 +1,69 @@
 package com.optimissa.training.transactionservice.controllers;
 
 import com.optimissa.training.transactionservice.dtos.Transactions;
-import com.optimissa.training.transactionservice.repository.TransactionRespository;
 import com.optimissa.training.transactionservice.services.TransactionsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionsController {
 
+    private static final Logger log = LoggerFactory.getLogger(TransactionsController.class);
+
+
     @Autowired
     private TransactionsService tranService;
 
     @GetMapping("/{id}")
     public Transactions getTransaction(@PathVariable int id) {
+        log.info("getTransaction: " + id);
 
-        return tranService.getByIdTransaction(id);
+        try{
+           return tranService.getByIdTransaction(id);
+        }catch (Exception e){
+            log.error("Error: " + e.getMessage());
+
+        }
+        return null;
+
+
     }
     @GetMapping("")
     public List<Transactions> getAllTransaction() {
+        log.info("getAllTransaction");
+        try{
+            return tranService.getAllTransactions();
+        }catch (Exception e){
+            log.error("Error: " + e.getMessage());
 
-        return tranService.getAllTransactions();
+        }
+
+        return null;
     }
 
     @PostMapping
-    public int createTransaction(@RequestBody Transactions transaction) {
+    public Object createTransaction(@RequestBody Transactions transaction) {
+        Long start=System.currentTimeMillis();
+        log.info("createTransaction: " + transaction);
+        try{
+            return tranService.insertNewTransaction(transaction);
+        }catch (Exception e){
+            log.error("Error: " + e.getMessage());
 
-        return tranService.insertNewTransaction(transaction);
+        }
+        Long end=System.currentTimeMillis();
+        log.info("Time: " + (end-start));
+
+        return null;
     }
+
+
+
+    
 
 }
