3288cebf638d6b16d86c0a320ccfe111df682974
OPTIMISSA\mario.munoz <mario.munoz@optimissa.com>
2023-04-27 09:43:43 +0200

    [Feature #28282] Exchange done. :)

--- currency-service/src/main/java/com/optimissa/training/currencyservice/conversion/ConversionService.java
+++ currency-service/src/main/java/com/optimissa/training/currencyservice/conversion/ConversionService.java
@@ -4,6 +4,7 @@ package com.optimissa.training.currencyservice.conversion;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,11 +15,14 @@ public class ConversionService {
     private RestTemplate restTemplate;
 
     @GetMapping
-    public String getLatestRates() {
-        String url = "https://api.frankfurter.app/latest?amount=&from=&to=";
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.getForObject(url, String.class);
-        return response;
+    public String convertCurrency(
+            @RequestParam("from") String from,
+            @RequestParam("to") String to,
+            @RequestParam("amount") Double amount
+    ) {
+        String url = "https://api.exchangerate.host/convert?from=" + from + "&to=" + to + "&amount=" + amount;
+        restTemplate = new RestTemplate();
+        return restTemplate.getForObject(url, String.class, from, to, amount);
     }
 
 }
