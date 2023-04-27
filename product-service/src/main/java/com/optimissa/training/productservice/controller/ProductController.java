3ff3d87c3d0f958859e279b1d56e717b73aa7d5c
Silvia Lopez <silvia.lopez@optimissa.com>
2023-04-27 09:24:14 +0200

    [#28275] added responseentity and logs

--- product-service/src/main/java/com/optimissa/training/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/optimissa/training/productservice/controller/ProductController.java
@@ -4,6 +4,8 @@ import com.optimissa.training.productservice.model.Product;
 import com.optimissa.training.productservice.service.ProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,50 +29,93 @@ public class ProductController {
     }
 
     @PostMapping("/create")
-    public int saveProduct (@RequestBody Product product) {
+    public ResponseEntity<String> saveProduct (@RequestBody Product product) {
         logger.info("Calling saveProduct()");
-        return productService.saveProduct(product);
-    }
+                int result = productService.saveProduct(product);
+                if (result > 0) {
+                    return ResponseEntity.ok("Product created successfully");
+                } else {
+                    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                            .body("Failed to create new product");
+                }
+        }
 
     @GetMapping("/get-all")
-    public List<Product> getAllProducts() {
+    public ResponseEntity<List<Product>> getAllProducts() {
         logger.info("Calling getAllProducts()");
-        return productService.getAllProducts();
+        List<Product> products = productService.getAllProducts();
+        if (products.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        }
     }
 
+
     @GetMapping("/get-by-id/{id}")
-    public Product getProductById (@PathVariable int id) {
+    public ResponseEntity<Product> getProductById(@PathVariable int id) {
         logger.info("Calling getProductById for id {}", id);
-        return productService.getProductById(id);
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        } else {
+            logger.error("Product with id {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/get-by-name/{name}")
-    public List<Product> getProductByName (@PathVariable String name) {
+    public ResponseEntity<List<Product>> getProductByName(@PathVariable String name) {
         logger.info("Calling getProductByName for {}", name);
-        return productService.getProductByName(name);
+        List<Product> products = productService.getProductByName(name);
+        if (!products.isEmpty()) {
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/get-all/active")
-    public List<Product> getActives () {
+    public ResponseEntity<List<Product>> getActives() {
         logger.info("Calling getActives");
-        return productService.getProductByActive();
+        List<Product> products = productService.getProductByActive();
+        if (!products.isEmpty()) {
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/get-all/inactive")
-    public List<Product> getInactives () {
+    public ResponseEntity<List<Product>> getInactives() {
         logger.info("Calling getInactives");
-        return productService.getProductByInactive();
+        List<Product> products = productService.getProductByInactive();
+        if (!products.isEmpty()) {
+            return new ResponseEntity<>(products, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/delete/{id}")
-    public int deleteProduct (@PathVariable int id) {
+    public ResponseEntity<String> deleteProduct(@PathVariable int id) {
         logger.info("Calling deleteProduct for id {}", id);
-        return productService.deleteProduct(id);
+        int result = productService.deleteProduct(id);
+        if (result > 0) {
+            return new ResponseEntity<>("Product with id " + id + " has been successfully deleted", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product with id " + id + " was not found", HttpStatus.NOT_FOUND);
+        }
     }
 
     @PutMapping("/update/{id}")
-    public int updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
+    public ResponseEntity<String> updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
         logger.info("Calling updateProduct for id {}", id);
-        return productService.updateProduct(id, product);
+        int result = productService.updateProduct(id, product);
+        if (result > 0) {
+            return new ResponseEntity<>("Product with id " + id + " has been successfully updated", HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Product with id " + id + " was not found", HttpStatus.NOT_FOUND);
+        }
     }
 }
