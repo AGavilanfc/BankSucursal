d5cead5d447abd7c064989c0c3ac77051e86fbfb
OPTIMISSA\javier.catalan <javier.catalan@optimissa.com>
2023-06-19 14:01:07 +0200

    fix client service

--- client-service/src/main/java/com/optimissa/training/clientservice/mappers/ClientResponseMapper.java
+++ client-service/src/main/java/com/optimissa/training/clientservice/mappers/ClientResponseMapper.java
@@ -7,12 +7,15 @@ public class ClientResponseMapper {
 
     public static ClientResponse mapToClientResponse(Client client){
         ClientResponse clientResponse = new ClientResponse();
+        clientResponse.setId(client.getId());
         clientResponse.setName(client.getName());
         clientResponse.setLastName1(client.getLastName1());
         clientResponse.setLastName2(client.getLastName2());
         clientResponse.setEmail(client.getEmail());
         clientResponse.setPhone(client.getPhone());
+        clientResponse.setActive(client.isActive());
         clientResponse.setIdentifier(client.getIdentifier());
+        clientResponse.setUser_id(client.getUserId());
         return clientResponse;
     }
 
